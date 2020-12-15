3767acd94afbb5b6216f867acfc38c25a140238d
George <George.tzhou22@gmail.com>
2020-12-14 19:19:30 -0500

    Finnhub api test

--- Finnhub/main.py
+++ Finnhub/main.py
@@ -18,3 +18,14 @@
 
 
 import finnhub
+
+# Setup client
+finnhub_client = finnhub.Client(api_key="sandbox_bv838gn48v6vtpa0emu0")
+
+# Stock candles
+res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
+print(res)
+
+#Convert to Pandas Dataframe
+import pandas as pd
+print(pd.DataFrame(res))
