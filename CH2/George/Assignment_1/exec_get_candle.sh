ef995d991dffefdfe524b1edb349a593e8b0026c
george <zhoutian4@gmail.com>
2021-01-11 17:24:16 -0500

    update sh command

--- CH2/George/Assignment_1/exec_get_candle.sh
+++ CH2/George/Assignment_1/exec_get_candle.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+cd /home/ubuntu/DS-Algo/CH2/George/Assignment_1/
 /home/ubuntu/anaconda3/bin/python -u /home/ubuntu/DS-Algo/CH2/George/Assignment_1/get_candle.py > /home/ubuntu/log/log_get_candle.log
